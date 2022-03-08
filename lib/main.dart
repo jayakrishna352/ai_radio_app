72010ed90277f7d2b779663a2778131eb90bf330
Jayakrishna <jayakrishnanalamolu@gmail.com>
2022-03-08 22:34:51 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AI Radio',
+      theme: ThemeData(
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/model/my_radio.dart
@@ -0,0 +1,27 @@
+class MyRadio {
+  final int id;
+  final int order;
+  final String name;
+  final String tagline;
+  final String color;
+  final String desc;
+  final String url;
+  final String category;
+  final String icon;
+  final String image;
+  final String lang;
+
+  MyRadio({
+    required this.name,
+    required this.tagline,
+    required this.color,
+    required this.desc,
+    required this.url,
+    required this.category,
+    required this.icon,
+    required this.image,
+    required this.lang,
+    required this.id,
+    required this.order,
+  });
+}
